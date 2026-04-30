48ec03a29b9fb52ee3de57bc7cff01cf7a561edc
Sofia Robles <robles@wustl.edu>
2026-04-30 13:36:42 -0500

    Initial Commit

---
+++ Animated_Storybook_App/Animated_Storybook_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Storybook_AppApp.swift
+//  Animated_Storybook_App
+//
+//  Created by Sofia Robles on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Storybook_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animated_Storybook_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animated_Storybook_App
+//
+//  Created by Sofia Robles on 4/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
